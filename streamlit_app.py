426f657323db69e6c86db9027dec25031f4aaa8f
Your Name <you@example.com>
2023-08-21 22:17:33 +0200

    select top1 from fruit_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,7 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header ("The fruit load list contains:")
 streamlit.dataframe (my_data_rows)
+
+# Allow the end user to add a fruit to the list
+fruit_choice2 = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thanks for adding jackfruit ', fruit_choice2)

5191b29731d016bb739855e1c32a2fccf6d60eeb
Your Name <you@example.com>
2023-08-21 23:27:38 +0200

    added button 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,6 +62,8 @@ def insert_row_snowflake(new_fruit):
         if new_fruit:
             my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
             return "Thanks for adding " + new_fruit
+        else: 
+            return ""
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):

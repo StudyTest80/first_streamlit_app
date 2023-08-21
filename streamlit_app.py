399fbc752baafe620b951e74a235d34e9913f981
Your Name <you@example.com>
2023-08-21 23:19:08 +0200

    added button 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,9 +55,6 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe (my_data_rows)
 
-# don't run anything past here while we troubleshoot
-streamlit.stop()
-
 # Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
@@ -72,3 +69,5 @@ if streamlit.button('Add a Fruit to the List'):
 #if add_my_fruit:
         #my_cur.execute("insert into fruit_load_list values ('" + add_my_fruit + "')")
     
+# don't run anything past here while we troubleshoot
+#streamlit.stop()

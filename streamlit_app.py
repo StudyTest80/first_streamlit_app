b9f21b2d514f2ec69bf0c401cc25b0a86768df4d
Your Name <you@example.com>
2023-08-21 23:38:52 +0200

    added button 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,20 +59,22 @@ if streamlit.button('Get Fruit List'):
 # Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        if new_fruit:
-            my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
-            return "Thanks for adding " + new_fruit
-        else: 
-            return ""
+        my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
+        return "Thanks for adding " + new_fruit
+
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+
 if streamlit.button('Add a Fruit to the List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets ["snowflake"])
-    back_from_function = insert_row_snowflake(add_my_fruit)
-    my_cnx.close()
-    streamlit.text (back_from_function)
+    if add_my_fruit:
+        my_cnx = snowflake.connector.connect(**streamlit.secrets ["snowflake"])
+        back_from_function = insert_row_snowflake(add_my_fruit)
+        my_cnx.close()
+        streamlit.text(back_from_function)
+
+
+
 #if add_my_fruit:
         #my_cur.execute("insert into fruit_load_list values ('" + add_my_fruit + "')")
-    
 # don't run anything past here while we troubleshoot
 #streamlit.stop()
